52ed8f2db37972d4a1f373cfc1f049980518a377
ivstarski <starivan@ya.ru>
2017-04-20 09:43:27 +0300

    Add balance

--- Cashex/viewmodel/MainViewmodel.cs
+++ Cashex/viewmodel/MainViewmodel.cs
@@ -12,36 +12,55 @@ namespace Cashex.viewmodel
     using System.Windows;
     using System.Windows.Input;
     using model;
+    using mvvm.x86;
     using support;
     using view;
 
     public class MainViewModel : DependencyObject
     {
+        public static readonly DependencyProperty BalanceProperty = DependencyProperty.Register(
+            "Balance", typeof( decimal ), typeof( MainViewModel ), new PropertyMetadata( default( decimal ) ) );
+
+        public decimal Balance
+        {
+            get { return (decimal)GetValue( BalanceProperty ); }
+            set { SetValue( BalanceProperty, value ); }
+        }
+
+        public static readonly DependencyProperty TotalProperty = DependencyProperty.Register(
+            "Total", typeof( string ), typeof( MainViewModel ), new PropertyMetadata( "1000000" ) );
+
+        public string Total
+        {
+            get { return (string)GetValue( TotalProperty ); }
+            set { SetValue( TotalProperty, value ); }
+        }
+
         public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
-            "Description", typeof(string), typeof(MainViewModel));
+            "Description", typeof( string ), typeof( MainViewModel ) );
 
         public string Description
         {
-            get { return (string)GetValue(DescriptionProperty); }
-            set { SetValue(DescriptionProperty, value); }
+            get { return (string)GetValue( DescriptionProperty ); }
+            set { SetValue( DescriptionProperty, value ); }
         }
 
         public static readonly DependencyProperty VolumeProperty = DependencyProperty.Register(
-            "Volume", typeof(string), typeof(MainViewModel));
+            "Volume", typeof( string ), typeof( MainViewModel ) );
 
         public string Volume
         {
-            get { return (string)GetValue(VolumeProperty); }
-            set { SetValue(VolumeProperty, value); }
+            get { return (string)GetValue( VolumeProperty ); }
+            set { SetValue( VolumeProperty, value ); }
         }
 
         public static readonly DependencyProperty SummaryProperty = DependencyProperty.Register(
-            "Summary", typeof(decimal), typeof(MainViewModel));
+            "Summary", typeof( decimal ), typeof( MainViewModel ) );
 
         public decimal Summary
         {
-            get { return (decimal)GetValue(SummaryProperty); }
-            set { SetValue(SummaryProperty, value); }
+            get { return (decimal)GetValue( SummaryProperty ); }
+            set { SetValue( SummaryProperty, value ); }
         }
 
         public ExpenseCollection Expenses { get; set; } = new ExpenseCollection();
@@ -49,34 +68,38 @@ namespace Cashex.viewmodel
         public ICommand DelCommand { get; set; }
         public ICommand EditCommand { get; set; }
 
-        public MainViewModel()
+        public MainViewModel( )
         {
-            Expenses.CollectionChanged += (sender, args) => { CalculateSummary(); };
-            AddCommand = new ActionCommand(() => Expenses.Add(Description, decimal.Parse(Volume.Trim())), CanAddExecute);
-            DelCommand = new ActionCommand(Remove);
-            EditCommand = new ActionCommand(Edit);
+            Expenses.CollectionChanged += ( sender, args ) => { CalculateSummary(); };
+            AddCommand = new ActionCommand( ( ) => Expenses.Add( Description, decimal.Parse( Volume.Trim() ) ), CanAddExecute );
+            DelCommand = new ActionCommand( Remove );
+            EditCommand = new ActionCommand( Edit );
             Expenses.Deserialize();
         }
 
-        private void CalculateSummary() => Summary = Expenses.Sum(model => model.Volume);
-
-        private bool CanAddExecute() =>
-            Expenses.FirstOrDefault(model => string.Equals(Description, model.Description)) == null
-            && !string.IsNullOrEmpty(Description) && !string.IsNullOrEmpty(Volume);
-
-        private void Remove(object o)
+        private void CalculateSummary( )
         {
-            var model = o as ExpenseModel;
-            if( model != null )
-                if( MessageBox.Show($"Действительно желаете удалить\n{o}?",
-                    "Удаление расхода", MessageBoxButton.YesNo,
-                    MessageBoxImage.Question) == MessageBoxResult.OK )
-                    Expenses.Remove((ExpenseModel)o);
+            Summary = Expenses.Sum( model => model.Volume );
+            Balance = decimal.Parse( Total ) - Summary;
         }
 
-        private void Edit(object o)
+        private bool CanAddExecute( ) =>
+            Expenses.FirstOrDefault( model => string.Equals( Description, model.Description ) ) == null
+            && !string.IsNullOrEmpty( Description ) && !string.IsNullOrEmpty( Volume );
+
+        private void Remove( object o )
         {
-            if( new EditWindow(o as ExpenseModel).ShowDialog() == true )
+            var model = o as ExpenseModel;
+            if ( model != null )
+                if ( MessageBox.Show( $"Действительно желаете удалить\n{o}?",
+                    "Удаление расхода", MessageBoxButton.YesNo,
+                    MessageBoxImage.Question ) == MessageBoxResult.Yes )
+                    Expenses.Remove( (ExpenseModel)o );
+        }
+
+        private void Edit( object o )
+        {
+            if ( new EditWindow( o as ExpenseModel ).ShowDialog() == true )
             {
                 Expenses.Serialize();
                 CalculateSummary();
